7fbc9df85cbcc778663c52426ec1fd5ca208392f
rushikeshsakharleofficial <73508010+rushikeshsakharleofficial@users.noreply.github.com>
2026-06-06 10:18:23 +0530

    Add lock package

---
+++ internal/lock/lock.go
@@ -0,0 +1,23 @@
+package lock
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"syscall"
+)
+
+func Acquire(path string) (*os.File, error) {
+	if err := os.MkdirAll(filepath.Dir(path), 0755); err != nil {
+		return nil, err
+	}
+	f, err := os.OpenFile(path, os.O_CREATE|os.O_RDWR, 0644)
+	if err != nil {
+		return nil, err
+	}
+	if err := syscall.Flock(int(f.Fd()), syscall.LOCK_EX|syscall.LOCK_NB); err != nil {
+		_ = f.Close()
+		return nil, fmt.Errorf("another logcut process may be running for this file: %w", err)
+	}
+	return f, nil
+}
