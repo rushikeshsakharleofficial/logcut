e77fb90f000af17ca780c4fdcf7a1e0b39d264b7
rushikeshsakharleofficial <73508010+rushikeshsakharleofficial@users.noreply.github.com>
2026-06-06 09:58:21 +0530

    Add runtime version flag support

---
+++ version.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+var version = "1.0.2"
+
+func init() {
+	for _, arg := range os.Args[1:] {
+		switch arg {
+		case "--version", "-version", "version":
+			fmt.Println("logcut", version)
+			os.Exit(0)
+		}
+	}
+}
