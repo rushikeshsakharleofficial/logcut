6a9282003304b4bd0fc99a7576df180005542495
rushikeshsakharleofficial <73508010+rushikeshsakharleofficial@users.noreply.github.com>
2026-06-06 10:45:50 +0530

    Support rate limit flags before CLI parse

--- cmd/logcut/main.go
+++ cmd/logcut/main.go
@@ -2,10 +2,34 @@ package main
 
 import (
 	"os"
+	"strings"
 
 	"github.com/rushikeshsakharleofficial/logcut/internal/cli"
 )
 
 func main() {
-	os.Exit(cli.Run(os.Args[1:]))
+	args := normalizeExtraFlags(os.Args[1:])
+	os.Exit(cli.Run(args))
+}
+
+func normalizeExtraFlags(args []string) []string {
+	out := make([]string, 0, len(args))
+	for i := 0; i < len(args); i++ {
+		arg := args[i]
+		switch {
+		case arg == "--rate-limit" && i+1 < len(args):
+			_ = os.Setenv("LOGCUT_RATE_LIMIT", args[i+1])
+			i++
+		case strings.HasPrefix(arg, "--rate-limit="):
+			_ = os.Setenv("LOGCUT_RATE_LIMIT", strings.TrimPrefix(arg, "--rate-limit="))
+		case arg == "--sleep-between-chunks" && i+1 < len(args):
+			_ = os.Setenv("LOGCUT_SLEEP_BETWEEN_CHUNKS", args[i+1])
+			i++
+		case strings.HasPrefix(arg, "--sleep-between-chunks="):
+			_ = os.Setenv("LOGCUT_SLEEP_BETWEEN_CHUNKS", strings.TrimPrefix(arg, "--sleep-between-chunks="))
+		default:
+			out = append(out, arg)
+		}
+	}
+	return out
 }
